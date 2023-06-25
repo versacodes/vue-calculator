fab505e27a45f06b162fa3d1a165cc656973be45
Franz Amersian <sianfranzsian@gmail.com>
2023-06-25 10:48:04 +0800

    Made button_contents const for .calc-btn text Added 4 more .btn-rows Increased font-size of .calc-btn text

--- src/components/CalculatorRow.vue
+++ src/components/CalculatorRow.vue
@@ -1,16 +1,32 @@
 <script setup>
 import {ref} from 'vue'
-const button_contents = ref(["C", "+/-", "%", 1])
+const button_contents = ref([["C", "+/-", "%", "/"], [7, 8, 9, "x"], [4, 5, 6, "-"], [1, 2, 3, "+"], [0, ".", "~", "="]])
 
 </script>
 
 <template>
   <div class="btn-row">
-    <button class="calc-btn" v-for="index in 3">{{index}}</button>
+    <button class="calc-btn" v-for="elem in button_contents[0]">{{elem}}</button>
+  </div>
+  <div class="btn-row">
+    <button class="calc-btn" v-for="elem in button_contents[1]">{{elem}}</button>
+  </div>
+  <div class="btn-row">
+    <button class="calc-btn" v-for="elem in button_contents[2]">{{elem}}</button>
+  </div>
+  <div class="btn-row">
+    <button class="calc-btn" v-for="elem in button_contents[3]">{{elem}}</button>
+  </div>
+  <div class="btn-row">
+    <button class="calc-btn" v-for="elem in button_contents[4]">{{elem}}</button>
   </div>
 </template>
 
 <style scoped>
+
+.btn-row ~ .btn-row {
+  margin-top: 7px;
+}
 
 .calc-btn {
   background: #666;
@@ -18,6 +34,7 @@ const button_contents = ref(["C", "+/-", "%", 1])
   height: 60px;
   border-radius: 50%;
   display: inline-block;
+  font-size: 18px;
 }
 
 .calc-btn:hover {
